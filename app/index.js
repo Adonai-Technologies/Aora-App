6a0d025ad4586f7c5d5d595b4eeb6126f400df28
Akua Konadu <akuakkonadu8@gmail.com>
2024-05-01 14:55:29 +0100

    menu

--- app/index.js
+++ app/index.js
@@ -1,15 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 export default function Page() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-         <Text className="text-3xl font-pblack">Welcome To Heaven Arena</Text>
-         <StatusBar style="auto" />
-         <Link href="/home" style={{color:'blue'}}>Go to home</Link>
-     </View>
-   
-  );
+	return (
+		<SafeAreaView className='bg-primary h-full'>
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className='w-full justify-center items-center h-full px-4'>
+					<Image
+						source={images.logo}
+						className='w-[130px] h-[84px]'
+						resizeMode='contain'
+					/>
+
+					<Image
+						source={images.cards}
+						className='max-w-[130px] w-full h-[300px]'
+						resizeMode='contain'
+					/>
+					<View className='relative mt-5'>
+						<Text className='text-3xl text-white font-bold text-center'>
+							Discover Endless Posisbilities with{" "}
+							<Text className='text-secondary-200'>VideoBible</Text>
+						</Text>
+						<Image
+							source={images.path}
+							className='w-[136px] h-[15px] absolute -bottom-2 -right-2'
+						/>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
-
-
