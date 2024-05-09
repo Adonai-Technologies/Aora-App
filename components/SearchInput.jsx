f53ead874d955455957429638a242a8ed1b6dfff
Akua Konadu <akuakkonadu8@gmail.com>
2024-05-09 17:24:42 +0100

    devering videos to the home page

---
+++ components/SearchInput.jsx
@@ -0,0 +1,37 @@
+import { View, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons, images } from "../constants";
+
+const SearchInput = ({
+	title,
+	value,
+	handelChangeText,
+	otherStyles,
+	keyboardType,
+	placeholder,
+	...props
+}) => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	return (
+		<View className='border-2 border-black-500 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4'>
+			<TextInput
+				className=' text-base mt-0.5 text-white flex-1 font-pregular'
+				value={value}
+				placeholder='Search For A Video Topic'
+				placeholderTextColor='#7b7b8b'
+				onChangeText={handelChangeText}
+				secureTextEntry={title === "Password" && !showPassword}
+			/>
+			<TouchableOpacity>
+                <Image
+                    source={icons.search}
+                    className='w-6 h-6'
+                    resizeMode="contain"
+                />
+            </TouchableOpacity>
+		</View>
+	);
+};
+
+export default SearchInput;
